0f902649ce87c40a3fa11a9cc50fd00cac70ad97
sammorrisdesign <mail@sammorrisdesign.co.uk>
2019-02-07 16:33:33 -0500

    fix js for real this time???

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
-console.log('inline');
-
 // Javascript that is inline. Should be used for anything that needs to be immediate
 import jquery from 'jquery';
 window.$ = jquery;
 
 import share from './modules/share.js';
+import shop from './modules/shop.js';
+import scroll from './modules/scroll.js';
 
 share.init();
+shop.init();
+scroll.init();
